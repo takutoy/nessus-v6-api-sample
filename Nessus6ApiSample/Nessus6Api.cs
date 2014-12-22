98864db6c571f75efc4ab8e9d092d53bb26aaffa
takutoy <takutoy@live.jp>
2014-12-22 22:47:37 +0900

    initial

---
+++ Nessus6ApiSample/Nessus6Api.cs
@@ -0,0 +1,214 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nessus6ApiSample
+{
+    public class Nessus6Api
+    {
+        string nessusip = null;
+        string token = null;
+
+        public Nessus6Api(string ipaddress)
+        {
+            nessusip = ipaddress;
+        }
+
+        async Task<JObject> Connect(string method, string resource, JObject param = null)
+        {
+            Uri uri = new Uri(string.Format("https://{0}:8834{1}", nessusip, resource));
+
+            using (HttpClient hc = new HttpClient())
+            {
+                if (token != null)
+                {
+                    hc.DefaultRequestHeaders.Add("X-Cookie", string.Format("token={0}", token));
+                }
+
+                switch (method.ToUpper())
+                {
+                    default:
+                        throw new ArgumentException();
+                    case "GET":
+                        var getjson = await hc.GetStringAsync(uri);
+                        return (JObject)JsonConvert.DeserializeObject(getjson);
+                    case "POST":
+                        HttpContent postcontent;
+                        if (param == null) postcontent = new StringContent("");
+                        else postcontent = new StringContent(param.ToString(), Encoding.UTF8, "application/json");
+                        var postresponse = await hc.PostAsync(uri, postcontent);
+                        var postrjson = (JObject)JsonConvert.DeserializeObject(await postresponse.Content.ReadAsStringAsync());
+                        return postrjson;
+                    case "PUT":
+                        var putresponse = await hc.PostAsync(uri, new StringContent(param.ToString(), Encoding.UTF8, "application/json"));
+                        var putjson = (JObject)JsonConvert.DeserializeObject(await putresponse.Content.ReadAsStringAsync());
+                        return putjson;
+                    case "DELETE":
+                        var deleteresponse = await hc.DeleteAsync(uri);
+                        var deletejson = (JObject)JsonConvert.DeserializeObject(await deleteresponse.Content.ReadAsStringAsync());
+                        return deletejson;
+                }
+            }
+        }
+
+        public async Task<string> LoginAsync(string login, string password)
+        {
+            JObject param = new JObject(
+                new JProperty("username", login),
+                new JProperty("password", password)
+                );
+            var json = await Connect("POST", "/session", param);
+
+            this.token = (string)json["token"];
+            return token;
+        }
+
+        public async Task LogoutAsync()
+        {
+            var response = await Connect("DELETE", "/session");
+            return;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>dictionary of policy title and uuid</returns>
+        public async Task<Dictionary<string, string>> GetPolicyTemplatesAsync()
+        {
+            var response = await Connect("GET", "/editor/policy/templates");
+            var data = response.SelectToken("templates").ToDictionary(_ => (string)_["title"], _ => (string)_["uuid"]);
+            return data;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="policyId"></param>
+        /// <param name="targets"></param>
+        /// <param name="description"></param>
+        /// <returns>scan id</returns>
+        public async Task<int> CreateScanAsync(string name, string policyId, string targets, string description = "")
+        {
+            JObject param = new JObject(
+                new JProperty("uuid", policyId),
+                new JProperty("settings", new JObject(
+                    new JProperty("name", name),
+                    new JProperty("description", description),
+                    new JProperty("text_targets", targets))));
+
+            var response = await Connect("POST", "/scans", param);
+
+            return (int)response["scan"]["id"];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scan_id"></param>
+        /// <returns>scan uuid</returns>
+        public async Task<string> LaunchScanAsync(int scan_id)
+        {
+            string resource = string.Format("/scans/{0}/launch", scan_id);
+            var response = await Connect("POST", resource);
+            return (string)response["scan_uuid"];
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scan_id"></param>
+        /// <returns>dictionary of scan uuid and history id</returns>
+        public async Task<Dictionary<string, int>> GetScanHistories(int scan_id)
+        {
+            string resource = string.Format("/scans/{0}", scan_id);
+            var response = await Connect("GET", resource);
+            var data = response.SelectToken("history").ToDictionary(_ => (string)_["uuid"], _ => (int)_["history_id"]);
+            return data;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scan_id"></param>
+        /// <returns>scan status string</returns>
+        public async Task<string> GetScanStatus(int scan_id, int history_id)
+        {
+            string resource = string.Format("/scans/{0}?history_id={1}", scan_id, history_id);
+            var response = await Connect("GET", resource);
+            var data = (string)response["info"]["status"];
+            return data;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scan_id"></param>
+        /// <param name="history_id"></param>
+        /// <param name="format"></param>
+        /// <returns>file id</returns>
+        public async Task<int> ExportScanAsync(int scan_id, int history_id = -1, string format = "nessus")
+        {
+            JObject param = new JObject(
+                new JProperty("format", format),
+                new JProperty("chapters", "vuln_hosts_summary")
+                );
+            if (history_id != -1)
+            {
+                param["history_id"] = history_id;
+            }
+
+            var response = await Connect("POST", string.Format("/scans/{0}/export", scan_id), param);
+            var fid = (int)response["file"];
+
+            while (true)
+            {
+                var statusresponse = await Connect("GET", string.Format("/scans/{0}/export/{1}/status", scan_id, fid));
+                if ((string)statusresponse["status"] != "loading") break;
+                await Task.Delay(1000);
+            }
+
+            return fid;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scan_id"></param>
+        /// <param name="fileId"></param>
+        /// <returns>content of export file</returns>
+        public async Task<byte[]> DownloadScanAsync(int scan_id, int fileId)
+        {
+            using (HttpClient hc = new HttpClient())
+            {
+                hc.DefaultRequestHeaders.Add("X-Cookie", string.Format("token={0}", token));
+                Uri uri = new Uri(string.Format("https://{0}:8834/scans/{1}/export/{2}/download", nessusip, scan_id, fileId));
+                var response = await hc.GetByteArrayAsync(uri);
+                return response;
+            }
+        }
+
+        /// <summary>
+        /// delete a scan or history if hid is specified
+        /// </summary>
+        /// <param name="sid"></param>
+        /// <param name="hid"></param>
+        /// <returns></returns>
+        public async Task DeleteScan(int scan_id, int history_id = -1)
+        {
+            string resource = history_id == -1 ?
+                string.Format("/scans/{0}", scan_id) :
+                string.Format("/scans/{0}/history/{1}", scan_id, history_id);
+
+            var response = await Connect("DELETE", resource);
+            return;
+        }
+    }
+}
--- Nessus6ApiSample/Program.cs
+++ Nessus6ApiSample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,47 @@ namespace Nessus6ApiSample
     {
         static void Main(string[] args)
         {
+            NessusTestAsync().Wait();
         }
+
+        static async Task NessusTestAsync()
+        {
+            // Ignore SSL validation
+            System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            string scanname = "test_" + DateTime.Now.ToString("yyyyMMddHHmmss");
+
+            var nessus = new Nessus6Api("192.168.248.129");
+            var token = await nessus.LoginAsync("admin", "password");
+
+            var policies = await nessus.GetPolicyTemplatesAsync();
+            var basic_network_scan_uuid = policies["Basic Network Scan"];
+
+            var scan_id = await nessus.CreateScanAsync(scanname, basic_network_scan_uuid, "127.0.0.1");
+            var scan_uuid = await nessus.LaunchScanAsync(scan_id);
+
+            var history_ids = await nessus.GetScanHistories(scan_id);
+            var history_id = history_ids[scan_uuid];
+
+            while (true)
+            {
+                var status = await nessus.GetScanStatus(scan_id, history_id);
+                if (status != "running") break;
+                await Task.Delay(1000 * 10);
+            }
+
+            var fid = await nessus.ExportScanAsync(scan_id, history_id, "nessus");
+            var download = await nessus.DownloadScanAsync(scan_id, fid);
+            File.WriteAllBytes(scanname + ".nessus", download);
+
+            var fidhtml = await nessus.ExportScanAsync(scan_id, history_id, "html");
+            var downloadhtml = await nessus.DownloadScanAsync(scan_id, fidhtml);
+            File.WriteAllBytes(scanname + ".html", downloadhtml);
+
+            await nessus.DeleteScan(scan_id);
+
+            await nessus.LogoutAsync();
+        }
+
     }
 }
